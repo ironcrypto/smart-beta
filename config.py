4b7d46377024ef4a5b3d022bc741d6266f3464ea
ironcrypto <31933810+ironcrypto@users.noreply.github.com>
2024-06-02 09:32:10 -0400

    Update config.py

--- config.py
+++ config.py
@@ -9,14 +9,14 @@ Created on Fri May  6 11:02:00 2022
 
 # define the core paramaters to BT 
 
-DATA_PATH = '/Users/stephanethomas/myML/smart-beta-bt/data'
+DATA_PATH = '~/smart-beta-bt/data'
 MCAP_FILE = 'nft_infra_coins_hist_mcaps.csv'
 PRICE_FILE = 'nft_infra_coins_hist_prices.csv'
 OHLC_FILE= 'nft_infra_coins_hist_ohlc.csv'
 UGSCORE_FILE='ugscores_bt.csv'
-OUTPUT_PATH = '/Users/stephanethomas/myML/smart-beta-bt/output'
-PROCESSED_PATH = '/Users/stephanethomas/myML/smart-beta-bt/data/processed'
-CPT_PATH = '/Users/stephanethomas/myML/smart-beta-bt/checkpoints'
+OUTPUT_PATH = '~/smart-beta-bt/output'
+PROCESSED_PATH = '~/smart-beta-bt/data/processed'
+CPT_PATH = '~/smart-beta-bt/checkpoints'
 
 # define the list of tokens in the index
 _TOKENINDEX = ['aavegotchi', 'audius', 'decentraland', 'dodo', 'enjincoin', 'nftx', 'rarible', 'superfarm',
